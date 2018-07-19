d74dfa0c27b4af7e8ed2ee3b894282c2e986a9ee
Unknown <guido.salcedo96@gmail.com>
2018-07-18 21:57:38 -0300

    Funciona Punto 2 - Falta punto  3 y ver XML

--- app/src/main/java/com/example/dh/entregableandroidguidosalcedo2/view/fragment/FragmentDetalle.java
+++ app/src/main/java/com/example/dh/entregableandroidguidosalcedo2/view/fragment/FragmentDetalle.java
@@ -4,20 +4,28 @@ package com.example.dh.entregableandroidguidosalcedo2.view.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.dh.entregableandroidguidosalcedo2.R;
+import com.example.dh.entregableandroidguidosalcedo2.controller.ControllerPintura;
 import com.example.dh.entregableandroidguidosalcedo2.model.pojo.Artista;
 import com.example.dh.entregableandroidguidosalcedo2.model.pojo.Pintura;
+import com.example.dh.entregableandroidguidosalcedo2.utils.ResultListener;
+import com.example.dh.entregableandroidguidosalcedo2.view.adapter.AdapterRecyclerViewPinturas;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,8 +37,8 @@ public class FragmentDetalle extends Fragment {
     private TextView textNacionalidad;
     private TextView textInfluencias;
     private Context context;
-
-    Pintura pinturaSeleccionada;
+    private Pintura pinturaSeleccionada;
+    private String idArtista;
 
     public FragmentDetalle() {
         // Required empty public constructor
@@ -49,9 +57,10 @@ public class FragmentDetalle extends Fragment {
         textNacionalidad = view.findViewById(R.id.textNacionalidad);
         textInfluencias = view.findViewById(R.id.textInfluencias);
 
-        String idArtista = unBundle.getString("posicion");
+        idArtista = unBundle.getString("idPintura");
 
-        leerArtista(idArtista);
+        leerArtista();
+        cargarPintura();
 
 
 
@@ -60,16 +69,17 @@ public class FragmentDetalle extends Fragment {
     }
 
 
-    public void leerArtista(String artistId){
+    public void leerArtista(){
         DatabaseReference mDatabase;
 
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         mDatabase = firebaseDatabase.getReference();
-        DatabaseReference referencePrimerMensaje = mDatabase.child("artists").child(artistId);
+        DatabaseReference referencePrimerMensaje = mDatabase.child("artists").child(idArtista);
 
         ValueEventListener valueEventListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                Toast.makeText(getContext(), "ENTRE", Toast.LENGTH_SHORT).show();
                 Artista mensajeLeido = dataSnapshot.getValue(Artista.class);
                 //Toast.makeText(context, mensajeLeido.toString(), Toast.LENGTH_SHORT).show();
                 //textTituloPintura.setText(pinturaSeleccionada.getName());
@@ -81,11 +91,27 @@ public class FragmentDetalle extends Fragment {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Toast.makeText(getContext(), "ddffdsfgd", Toast.LENGTH_SHORT).show();
             }
         };
-        referencePrimerMensaje.addListenerForSingleValueEvent(valueEventListener);
-    }
 
+        referencePrimerMensaje.addListenerForSingleValueEvent(valueEventListener);
+//        referencePrimerMensaje.addValueEventListener(valueEventListener);
+    }
+    public void cargarPintura() {
+        ControllerPintura noticiaController = new ControllerPintura();
+        noticiaController.obtenerPintura(new ResultListener<List<Pintura>>() {
+            @Override
+            public void finish(List<Pintura> resultado) {
+
+                for (Pintura pinturafor : resultado) {
+                    if (pinturafor.getArtistId().equals(idArtista)) {
+                        textTituloPintura.setText(pinturafor.getName());
+                    }
+                }
+
+            }
+        });
+    }
 
 }
